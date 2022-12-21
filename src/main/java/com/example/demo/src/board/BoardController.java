841e81b074c3a1dfe2a692b81617b74e764016ed
zzansol <chsk9912@gmail.com>
2022-12-21 23:26:21 +0900

    User Complete

---
+++ src/main/java/com/example/demo/src/board/BoardController.java
@@ -0,0 +1,22 @@
+package com.example.demo.src.board;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.user.model.*;
+import com.example.demo.utils.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+import static com.example.demo.config.BaseResponseStatus.*;
+import static com.example.demo.utils.ValidationRegex.isRegexEmail;
+
+@RestController
+@RequestMapping("/app/boards")
+public class BoardController {
+
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+}
